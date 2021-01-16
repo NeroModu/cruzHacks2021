0d0369e21862af2c3bd76ac8f3e39e579db5401f
Enrique <edavalos@ucsc.edu>
2021-01-16 11:51:43 -0800

    Added google button

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,5 +10,8 @@ new Vue({
   router,
   store,
   vuetify,
+  icons: {
+    iconfont: 'mdi'
+  },
   render: h => h(App)
 }).$mount('#app')
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <div>
+      <img alt="Vue logo" src="../assets/logo.png">
+    </div>
+    <div>
+      <v-btn
+        depressed
+        color="primary"
+      >
+        <v-icon
+          dark
+          left
+        >
+          mdi-google
+        </v-icon>
+        Sign In
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -11,3 +27,9 @@ export default {
   name: 'Home',
 }
 </script>
+
+<style scoped>
+.home {
+  text-align: center;
+}
+</style>
