75c1601b51eff6af16c870bd2399f0d2745d8beb
Ezra Moussa <ezra.moussa@gmail.com>
2021-01-16 21:01:13 +0000

    contractHandlerupdate

--- VaaS/app.js
+++ VaaS/app.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // [START gae_node_request_example]
+const contractHandler = require('./contractHandler.js');
 const express = require('express');
 const crypto = require('crypto');
+const hmac = "asjdhg374y3uiewh";
 
 const app = express();
 
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
 app.get('/hash/:id', (req, res) => {
     let secret = req.params.id;
 
-    let hash = crypto.createHmac("sha256", "thisismysecretkey").update(secret).digest("hex");
+    let hash = crypto.createHmac("sha256", "thisismysecretkey").update(hmac).digest("hex");
 
     res.status(200).send(hash).end;
 });
@@ -23,8 +25,6 @@ app.get('/vote/', (req, res) =>{
 });
 
 
-
-
 // Start the server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
@@ -32,6 +32,8 @@ app.listen(PORT, () => {
   console.log('Press Ctrl+C to quit.');
 });
 // [END gae_node_request_example]
+
+contractHandler.getStandings();
 
 module.exports = app;
 
