a3d838213533fbdebed3c9546710b2f7d5d3954c
Ezra Moussa <ezra.moussa@gmail.com>
2021-01-17 09:05:34 +0000

    modified nameserver proxy

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,6 +3,8 @@ module.exports = {
     "vuetify"
   ],
   "devServer": {
-    "public": "8080-cs-286288504737-default.us-west1.cloudshell.dev"
+    "public": "8080-cs-286288504737-default.us-west1.cloudshell.dev",
+    "public": "vaas.edavalos.com",
+    "public": "vaas.ezramoussa.com"
   }  
 }
